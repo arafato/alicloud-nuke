687ddb69fe400912f2896fc221e1f0155918f097
arafato <arafato@outlook.com>
2026-01-22 19:42:31 +0100

    updated

--- go.mod
+++ go.mod
@@ -3,11 +3,23 @@ module github.com/arafato/ali-nuke
 go 1.24.0
 
 require (
+	github.com/alibabacloud-go/alb-20200616/v2 v2.3.1
+	github.com/alibabacloud-go/cr-20181201/v2 v2.5.0
 	github.com/alibabacloud-go/darabonba-openapi/v2 v2.1.14
+	github.com/alibabacloud-go/dds-20151201/v4 v4.2.0
 	github.com/alibabacloud-go/ecs-20140526/v7 v7.5.1
+	github.com/alibabacloud-go/ess-20220222/v2 v2.12.1
+	github.com/alibabacloud-go/nas-20170626/v3 v3.5.2
+	github.com/alibabacloud-go/nlb-20220430/v2 v2.0.3
+	github.com/alibabacloud-go/polardb-20170801/v6 v6.15.0
+	github.com/alibabacloud-go/r-kvstore-20150101/v4 v4.0.1
+	github.com/alibabacloud-go/rds-20140815/v4 v4.2.1
+	github.com/alibabacloud-go/slb-20140515/v4 v4.0.13
 	github.com/alibabacloud-go/tea v1.3.13
 	github.com/alibabacloud-go/vpc-20160428/v6 v6.16.0
+	github.com/aliyun/alibabacloud-oss-go-sdk-v2 v1.3.0
 	github.com/cenkalti/backoff/v5 v5.0.3
+	github.com/fatih/color v1.18.0
 	github.com/olekukonko/tablewriter v1.0.9
 	github.com/spf13/cobra v1.10.1
 	golang.org/x/sync v0.17.0
@@ -18,12 +30,10 @@ require (
 	github.com/alibabacloud-go/alibabacloud-gateway-spi v0.0.5 // indirect
 	github.com/alibabacloud-go/debug v1.0.1 // indirect
 	github.com/alibabacloud-go/endpoint-util v1.1.0 // indirect
-	github.com/alibabacloud-go/nas-20170626/v3 v3.5.2 // indirect
 	github.com/alibabacloud-go/openapi-util v0.1.1 // indirect
 	github.com/alibabacloud-go/tea-utils/v2 v2.0.7 // indirect
 	github.com/aliyun/credentials-go v1.4.5 // indirect
 	github.com/clbanning/mxj/v2 v2.7.0 // indirect
-	github.com/fatih/color v1.18.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
@@ -38,5 +48,6 @@ require (
 	github.com/tjfoc/gmsm v1.4.1 // indirect
 	golang.org/x/net v0.26.0 // indirect
 	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/time v0.4.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 )
--- utils/client.go
+++ utils/client.go
@@ -1,11 +1,22 @@
 package utils
 
 import (
+	alb "github.com/alibabacloud-go/alb-20200616/v2/client"
+	cr "github.com/alibabacloud-go/cr-20181201/v2/client"
 	openapi "github.com/alibabacloud-go/darabonba-openapi/v2/client"
+	dds "github.com/alibabacloud-go/dds-20151201/v4/client"
 	ecs "github.com/alibabacloud-go/ecs-20140526/v7/client"
+	ess "github.com/alibabacloud-go/ess-20220222/v2/client"
 	nas "github.com/alibabacloud-go/nas-20170626/v3/client"
+	nlb "github.com/alibabacloud-go/nlb-20220430/v2/client"
+	polardb "github.com/alibabacloud-go/polardb-20170801/v6/client"
+	r_kvstore "github.com/alibabacloud-go/r-kvstore-20150101/v4/client"
+	rds "github.com/alibabacloud-go/rds-20140815/v4/client"
+	slb "github.com/alibabacloud-go/slb-20140515/v4/client"
 	"github.com/alibabacloud-go/tea/tea"
 	vpc "github.com/alibabacloud-go/vpc-20160428/v6/client"
+	"github.com/aliyun/alibabacloud-oss-go-sdk-v2/oss"
+	"github.com/aliyun/alibabacloud-oss-go-sdk-v2/oss/credentials"
 
 	"github.com/arafato/ali-nuke/types"
 )
@@ -44,4 +55,104 @@ func CreateNASClient(creds *types.Credentials, region string) (*nas.Client, erro
 	}
 
 	return nas.NewClient(config)
+}
+
+// CreateESSClient creates an Auto Scaling (ESS) client for a specific region
+func CreateESSClient(creds *types.Credentials, region string) (*ess.Client, error) {
+	config := &openapi.Config{
+		AccessKeyId:     tea.String(creds.AccessKeyID),
+		AccessKeySecret: tea.String(creds.AccessKeySecret),
+		RegionId:        tea.String(region),
+	}
+	return ess.NewClient(config)
+}
+
+// CreateCRClient creates a Container Registry client for a specific region
+func CreateCRClient(creds *types.Credentials, region string) (*cr.Client, error) {
+	config := &openapi.Config{
+		AccessKeyId:     tea.String(creds.AccessKeyID),
+		AccessKeySecret: tea.String(creds.AccessKeySecret),
+		RegionId:        tea.String(region),
+	}
+	return cr.NewClient(config)
+}
+
+// CreateSLBClient creates a Classic Load Balancer (SLB) client for a specific region
+func CreateSLBClient(creds *types.Credentials, region string) (*slb.Client, error) {
+	config := &openapi.Config{
+		AccessKeyId:     tea.String(creds.AccessKeyID),
+		AccessKeySecret: tea.String(creds.AccessKeySecret),
+		RegionId:        tea.String(region),
+	}
+	return slb.NewClient(config)
+}
+
+// CreateALBClient creates an Application Load Balancer (ALB) client for a specific region
+func CreateALBClient(creds *types.Credentials, region string) (*alb.Client, error) {
+	config := &openapi.Config{
+		AccessKeyId:     tea.String(creds.AccessKeyID),
+		AccessKeySecret: tea.String(creds.AccessKeySecret),
+		RegionId:        tea.String(region),
+	}
+	return alb.NewClient(config)
+}
+
+// CreateNLBClient creates a Network Load Balancer (NLB) client for a specific region
+func CreateNLBClient(creds *types.Credentials, region string) (*nlb.Client, error) {
+	config := &openapi.Config{
+		AccessKeyId:     tea.String(creds.AccessKeyID),
+		AccessKeySecret: tea.String(creds.AccessKeySecret),
+		RegionId:        tea.String(region),
+	}
+	return nlb.NewClient(config)
+}
+
+// CreateRDSClient creates an RDS client for a specific region
+func CreateRDSClient(creds *types.Credentials, region string) (*rds.Client, error) {
+	config := &openapi.Config{
+		AccessKeyId:     tea.String(creds.AccessKeyID),
+		AccessKeySecret: tea.String(creds.AccessKeySecret),
+		RegionId:        tea.String(region),
+	}
+	return rds.NewClient(config)
+}
+
+// CreateRedisClient creates a Redis (KVStore) client for a specific region
+func CreateRedisClient(creds *types.Credentials, region string) (*r_kvstore.Client, error) {
+	config := &openapi.Config{
+		AccessKeyId:     tea.String(creds.AccessKeyID),
+		AccessKeySecret: tea.String(creds.AccessKeySecret),
+		RegionId:        tea.String(region),
+	}
+	return r_kvstore.NewClient(config)
+}
+
+// CreateMongoDBClient creates a MongoDB (DDS) client for a specific region
+func CreateMongoDBClient(creds *types.Credentials, region string) (*dds.Client, error) {
+	config := &openapi.Config{
+		AccessKeyId:     tea.String(creds.AccessKeyID),
+		AccessKeySecret: tea.String(creds.AccessKeySecret),
+		RegionId:        tea.String(region),
+	}
+	return dds.NewClient(config)
+}
+
+// CreatePolarDBClient creates a PolarDB client for a specific region
+func CreatePolarDBClient(creds *types.Credentials, region string) (*polardb.Client, error) {
+	config := &openapi.Config{
+		AccessKeyId:     tea.String(creds.AccessKeyID),
+		AccessKeySecret: tea.String(creds.AccessKeySecret),
+		RegionId:        tea.String(region),
+	}
+	return polardb.NewClient(config)
+}
+
+// CreateOSSClient creates an OSS client for a specific region
+func CreateOSSClient(creds *types.Credentials, region string) (*oss.Client, error) {
+	provider := credentials.NewStaticCredentialsProvider(creds.AccessKeyID, creds.AccessKeySecret)
+	cfg := oss.LoadDefaultConfig().
+		WithCredentialsProvider(provider).
+		WithRegion(region)
+
+	return oss.NewClient(cfg), nil
 }
